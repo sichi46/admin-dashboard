f416e918055ec9df9f58b50191dbe5147b0b8b44
Sichi46 <willsonmulenga7@gmail.com>
2024-08-21 14:59:55 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ProductPage from "./pages/ProductPage";
+import OverviewPage from "./pages/OverviewPage";
+import SideBar from "./components/SideBar";
+
+const App = () => {
+  return (
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      {/* Background styles */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-500  via-gray-800 to-gray-900 opacity-80" />
+        <div className="absolute inset-0 backdrop-blur-sm" />
+      </div>
+
+      <SideBar />
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/products" element={<ProductPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
