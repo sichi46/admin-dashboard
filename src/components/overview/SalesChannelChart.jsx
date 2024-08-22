74fc2f1360d8e97ebc42dd7565db4d406b014879
Sichi46 <willsonmulenga7@gmail.com>
2024-08-22 11:54:58 +0200

    changes

--- src/components/overview/SalesChannelChart.jsx
+++ src/components/overview/SalesChannelChart.jsx
@@ -1,7 +1,62 @@
+import { motion } from "framer-motion";
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Cell,
+} from "recharts";
 import React from "react";
+const COLORS = ["#6366f1", "#8b5cf6", "#ec4899", "#10B981", "#f59e0b"];
+const SALES_CHANNEL_DATA = [
+  { name: "Website", value: 45600 },
+  { name: "Mobile App", value: 38200 },
+  { name: "Market Place", value: 29800 },
+  { name: "Social Media", value: 18700 },
+];
 
 const SalesChannelChart = () => {
-  return <div></div>;
+  return (
+    <motion.div
+      className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 lg:col-span-2 border border-gray-700 "
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.4 }}
+    >
+      <h2 className="text-lg mb-4 font-medium text-gray-100">
+        Sales by Channel
+      </h2>
+      <div className="h-80">
+        <ResponsiveContainer>
+          <BarChart data={SALES_CHANNEL_DATA}>
+            <CartesianGrid strokeDasharray="3 3" stroke="#4b5563" />
+            <XAxis dataKey="name" stroke="#9ca3af" />
+            <YAxis stroke="#9ca3af" />
+            <Tooltip
+              contentStyle={{
+                backgroundColor: "rgba(31,41,55,.8)",
+                borderColor: "#4b5563",
+              }}
+              itemStyle={{ color: "#e5e7EB" }}
+            />
+            <Legend />
+            <Bar dataKey={"value"} fill="#8884d8">
+              {SALES_CHANNEL_DATA.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </motion.div>
+  );
 };
 
 export default SalesChannelChart;
