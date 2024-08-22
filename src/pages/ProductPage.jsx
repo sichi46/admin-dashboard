35bc8573cb47a9dee7057e55602367fee48867ee
Sichi46 <willsonmulenga7@gmail.com>
2024-08-22 12:40:04 +0200

    changes -added the stat cards on the product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -12,7 +12,7 @@ const ProductPage = () => {
         {/* STATS */}
         <motion.div
           className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
-          intitial={{ opacity: 0, y: 20 }}
+          initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
         >
